562c3e827ce095017e3fa5f761f3224d0349baab
annatisch <antisch@microsoft.com>
2015-07-01 16:32:15 +1200

    Started work on custom exceptions

---
+++ MayaClient/batchapps_maya/scripts/exceptions.py
@@ -0,0 +1,51 @@
+#-------------------------------------------------------------------------
+#
+# Batch Apps Maya Plugin
+#
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#--------------------------------------------------------------------------
+
+import traceback
+import logging
+LOG = logging.getLogger('BatchAppsMaya')
+
+class BatchMayaException(Exception):
+
+    def __init__(self, *args):
+        super(BatchMayaException, self).__init__(*args)
+
+class MayaApiException(BatchMayaException):
+
+    def __init__(self, *args):
+        LOG.debug("MayaAPIException: {0}".format(self))
+        super(MayaApiException, self).__init__(*args)
+
+class SessionExpiredException(BatchMayaException):
+    pass
+
+class CancellationException(BatchMayaException):
+
+    def __init__(self, *args):
+        LOG.info("Process cancelled by user.")
+        super(MayaApiException, self).__init__(*args)
