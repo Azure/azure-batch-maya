cc82a080dc57af4099cfadbe6c46f19bd0921138
David Kydd <dakydd@microsoft.com>
2018-01-31 15:09:32 +1300

    Duplicate code out of config.py into job_watcher to support AAD. #refactor this later

--- azure_batch_maya/scripts/tools/job_watcher.py
+++ azure_batch_maya/scripts/tools/job_watcher.py
@@ -13,6 +13,12 @@ import sys
 import os
 import re
 import threading
+import json
+import traceback
+import datetime
+import dateutil.tz
+from dateutil.parser import parse as dateparse
+from azure.batch.models import BatchErrorException
 
 try:
     str = unicode
@@ -21,6 +27,12 @@ except NameError:
 batch_client = None
 storage_client = None
 header_line_length = 50
+
+batchAadResource = "https://batch.core.windows.net/"
+mgmtAadResource = "https://management.core.windows.net/"
+aadTenant = "microsoft.onmicrosoft.com"
+aadAuthorityHostUrl = "https://login.microsoftonline.com"
+aadClientId = "04b07795-8ddb-461a-bbee-02f9e1bf7b46" #Azure CLI
 
 
 def header(header):
@@ -130,20 +142,104 @@ def track_job_progress(job_id, dwnld_dir):
     except KeyboardInterrupt:
         raise RuntimeError("Monitoring aborted.")
 
+def convert_utc_expireson_to_local_timezone_naive(token):
+    #the standard token expireson format which the various AAD libraries expect / return is a vanilla datetime string, in local time and timezone naive (no tz specified)
+    localtz = dateutil.tz.tzlocal()
+    expireson_utc = dateparse(token['expiresOnUTC']).replace(tzinfo = dateutil.tz.gettz('UTC'))
+    expireson_local = expireson_utc.astimezone(dateutil.tz.tzlocal())
+    expireson_local_tz_naive = expireson_local.replace(tzinfo = None)
+    token['expiresOn'] = str(expireson_local_tz_naive)
+    del token['expiresOnUTC']
+
+def need_to_refresh_auth_tokens(auth_token_list):
+
+    currentTime = datetime.datetime.now()
+
+    tokenRefreshThresholdSeconds = 5 * 60
+
+    for token in auth_token_list:
+        if (dateparse(token['expiresOn']) - currentTime).total_seconds() < tokenRefreshThresholdSeconds:
+            return True
+    return False
+
+def refresh_auth_tokens(mgmt_token, batch_token):
+
+    context = adal.AuthenticationContext(aadAuthorityHostUrl + '/' + aadTenant, api_version=None)
+
+    mgmt_auth_token = context.acquire_token_with_refresh_token(
+        mgmt_token['refreshToken'],
+        aadClientId,
+        mgmtAadResource)
+
+    batch_auth_token =  context.acquire_token_with_refresh_token(
+        batch_token['refreshToken'], 
+        aadClientId,
+        batchAadResource)
+    
+    return mgmt_auth_token, batch_auth_token
+
+def call(command, *args, **kwargs):
+    """Wrap all Batch and Storage API calls in order to handle errors.
+    Some errors we anticipate and raise without a dialog (e.g. PoolNotFound).
+    Others we raise and display to the user.
+    """
+    try:
+        return command(*args, **kwargs)
+    except BatchErrorException as exp:
+        if exp.error.code in ACCEPTED_ERRORS:
+            print "Call failed: {}".format(exp.error.code)
+            raise
+        else:
+            message = exp.error.message.value
+            if exp.error.values:
+                message += "Details:\n"
+                for detail in exp.error.values:
+                    message += "{}: {}".format(detail.key, detail.value)
+            raise ValueError(message)
+    except Exception as exp:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+        raise ValueError("Error: {0}".format(exp))
+
 
 def _authenticate(cfg_path):
     global batch_client, storage_client
     cfg = ConfigParser.ConfigParser()
     try:
         cfg.read(cfg_path)
-        credentials = SharedKeyCredentials(
-                cfg.get("AzureBatch", "batch_account"),
-                cfg.get("AzureBatch", "batch_key"))
+
+        #TODO refactor all this to share code / functions with "config"
+
+        subscription_id = cfg.get('AzureBatch', 'subscription_id')
+        batch_url = cfg.get('AzureBatch', 'batch_url')
+
+        mgmt_auth_token = json.loads(cfg.get('AzureBatch', 'mgmt_auth_token'))
+        convert_utc_expireson_to_local_timezone_naive(mgmt_auth_token)
+
+        batch_auth_token = json.loads(cfg.get('AzureBatch', 'batch_auth_token'))
+        convert_utc_expireson_to_local_timezone_naive(batch_auth_token)
+
+        if need_to_refresh_auth_tokens([mgmt_auth_token, batch_auth_token]):
+            mgmt_auth_token, batch_auth_token = refresh_auth_tokens(mgmt_auth_token, batch_auth_token)
+
+        mgmtCredentials = AADTokenCredentials(mgmt_auth_token)
+        batchCredentials = AADTokenCredentials(batch_auth_token)
+
+        storage_account_resource_id = cfg.get('AzureBatch', 'storage_account_resource_id')
+
+        parsedStorageAccountId = msrestazuretools.parse_resource_id(storage_account_resource_id)
+        storage_account = parsedStorageAccountId['name']
+
+        storage_mgmt_client = StorageManagementClient(mgmtCredentials, subscription_id)
+
+        storage_key = call(storage_mgmt_client.storage_accounts.list_keys, parsedStorageAccountId['resource_group'], storage_account).keys[0].value
+
         storage_client = storage.BlockBlobService(
-            cfg.get("AzureBatch", "storage_account"),
-            cfg.get("AzureBatch", "storage_key"))
-        batch_client = batch.BatchExtensionsClient(
-            credentials, base_url=cfg.get("AzureBatch", "batch_url"),
+            storage_account,
+            storage_key)
+
+        batch_client = batch.BatchExtensionsClient(batchCredentials, 
+            base_url=batch_url,
             storage_client=storage_client)
         try:
             batch_client.threads = cfg.get("AzureBatch", "threads")
@@ -164,6 +260,15 @@ if __name__ == "__main__":
         import azure.storage.blob as storage
         import azure.batch_extensions as batch
         from azure.batch.batch_auth import SharedKeyCredentials
+
+        from msrestazure.azure_active_directory import AdalAuthentication
+        from msrestazure.azure_active_directory import AADTokenCredentials
+        import msrestazure.tools as msrestazuretools
+
+        from azure.mgmt.resource.subscriptions import SubscriptionClient
+        from azure.mgmt.batch import BatchManagementClient
+        from azure.mgmt.storage import StorageManagementClient
+
         data_path = sys.argv[1].decode('utf-8')
         job_id = sys.argv[2]
         download_dir = sys.argv[3].decode('utf-8')
